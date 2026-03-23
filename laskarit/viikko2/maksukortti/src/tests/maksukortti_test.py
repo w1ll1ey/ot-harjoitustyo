5733c537c437269e904f245a6669f24514a2a4a6
w1ll1ey <vili.ilari.kaisto@gmail.com>
2026-03-23 12:58:32 +0200

    add task 3 week 2

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -24,6 +24,12 @@ class TestMaksukortti(unittest.TestCase):
         kortti.syo_edullisesti()
 
         self.assertEqual(kortti.saldo_euroina(), 2.0)
+        
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(350)
+        kortti.syo_maukkaasti()
+        
+        self.assertEqual(kortti.saldo_euroina(), 3.5)
     
     def test_kortille_voi_ladata_rahaa(self):
         self.kortti.lataa_rahaa(2500)
@@ -34,3 +40,20 @@ class TestMaksukortti(unittest.TestCase):
         self.kortti.lataa_rahaa(20000)
 
         self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+        
+    def test_negatiivinen_summa_ei_muuta_saldoa(self):
+        self.kortti.lataa_rahaa(-1000)
+        
+        self.assertEqual(self.kortti.saldo_euroina(), 10.00)
+        
+    def test_kortilla_pystyy_ostamaan_tasarahalla_edullisen(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+        
+        self.assertEqual(kortti.saldo_euroina(), 0.00)
+        
+    def test_kortilla_pystyy_ostamaan_tasarahalla_maukkaan(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+        
+        self.assertEqual(kortti.saldo_euroina(), 0.00)
