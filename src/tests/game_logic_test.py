b3127d7a3ec09f410fb4543f9023e875dbf80f93
w1ll1ey <vili.ilari.kaisto@gmail.com>
2026-04-16 14:45:07 +0300

    update current tests

--- src/tests/game_logic_test.py
+++ src/tests/game_logic_test.py
@@ -6,13 +6,18 @@ from entities.player import Player
 
 class TestGameLogic(unittest.TestCase):
     def setUp(self):
-        level = Level([
+        self.game = GameLogic()
+        test_map = Level([
             [1, 1, 1, 1],
             [1, 0, 0, 1],
             [1, 1, 1, 1]
         ])
-        player = Player(1, 1, 5)
-        self.game = GameLogic(level, player)
+        test_player = Player(1, 1, 5, "Test")
+        
+        self.game.level = test_map
+        self.game.player = test_player
+        self.game.enemies = []
+        
 
     def test_move_player_valid_move_changes_coordinates(self):
         self.game.move_player(1, 0)
